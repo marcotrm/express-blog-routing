37d6ea7e85f9ea3555ba866575086a1b539daecd
marco torremacco <mtorremacco@gmail.comm>
2025-01-21 13:41:32 +0100

    feat add CRUD

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./router/posts');
+
+app.use('/posts', postsRouter);
 
 app.use(express.static('public'))
 
--- routers/post.js
+++ routers/post.js
@@ -4,33 +4,70 @@ const router = express.Router();
 const posts = 
         [
             {
+              id: 1,  
               titolo: "I Benefici dell'Allenamento a Circuito",
               contenuto: "Scopri come l'allenamento a circuito può migliorare la tua forza e resistenza.",
               immagine: "/img/pasta_barbabietola.jpeg",
               tags: ["allenamento", "fitness", "salute"]
             },
             {
+              id: 2,  
               titolo: "Ricetta di Smoothie Proteico",
               contenuto: "Una ricetta semplice e veloce per un delizioso smoothie proteico post-allenamento.",
               immagine: "/img/ciambellone.jpeg",
               tags: ["ricetta", "nutrizione", "proteine"]
             },
             {
+              id: 3,  
               titolo: "Guida all'HTML per Principianti",
               contenuto: "Impara le basi di HTML per creare il tuo primo sito web.",
               immagine: "/img/pane_fritto_dolce.jpeg",
               tags: ["programmazione", "HTML", "web development"]
             },
             {
+              id: 4,  
               titolo: "I Migliori Esercizi per i Bicipiti",
               contenuto: "Una selezione dei migliori esercizi per sviluppare bicipiti forti e definiti.",
               immagine: "/img/cracker_barbabietola.jpeg",
               tags: ["allenamento", "bicipiti", "muscoli"]
             },
             {
+              id: 5,  
               titolo: "5 Consigli per Dormire Meglio",
               contenuto: "Suggerimenti pratici per migliorare la qualità del sonno e sentirsi più energici.",
               immagine: "/img/torta_paesana.jpegpost",
               tags: ["salute", "benessere", "sonno"]
             }
           ]
+
+          router.get('/', (req, res) => {
+            res.json(posts);
+            });
+            // show
+            router.get('/:id', (req, res) => {
+                if(isNaN(req.params.id)) {
+                    return res.sendStatus(400)
+                }
+                    if(posts){
+                        res.json(posts)
+                    } else {
+                        res.sendStatus(404)
+                    }
+            });
+            // store
+            router.post('/', (req, res) => {
+                res.send('Creazione di un nuovo post');
+            });
+            // update
+            router.put('/:id', (req, res) => {
+                res.send('Modifica integrale del post');
+            });
+            // modify
+            router.patch('/:id', (req, res) => {
+                res.send('Modifica parziale del post')
+            });
+            // destroy
+            router.delete('/:id', (req, res) => {
+                res.send('Eliminazione del post');
+        });
+module.exports = router;
